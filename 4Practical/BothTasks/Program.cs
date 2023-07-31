57f7c5c7c9b7580a23b980fc3c7b7e35bbf870e6
Dmitriy Smirnov <dima-smirnov-132003@mail.ru>
2023-07-31 12:02:53 +0300

    Complete main tasks

--- 4Practical/BothTasks/Program.cs
+++ 4Practical/BothTasks/Program.cs
@@ -14,15 +14,40 @@ namespace BothTasks
             int rows = int.Parse(Console.ReadLine());
             int cols = int.Parse(Console.ReadLine());
             Random rand= new Random();
-            Console.WriteLine($"Матрица {rows} на {cols}:");
-            int[,] matrix = new int[rows, cols];
+            int sum = 0;
+            Console.WriteLine($"Матрица A {rows} на {cols}:");
+            int[,] matrixA = new int[rows, cols];
             for(int i=0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrixA[i, j] = rand.Next(100);
+                    Console.Write($"{matrixA[i, j],2} ");
+                    sum += matrixA[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сумма элементов матрицы А - " + sum);
+            Console.WriteLine($"Матрица B {rows} на {cols}:");
+            int[,] matrixB = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrixB[i, j] = rand.Next(100);
+                    Console.Write($"{matrixB[i, j],2} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Матрица суммы А и В: ");
+            int[,] matrixC = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
             {
                 Console.WriteLine();
                 for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, j] = rand.Next(100);
-                    Console.Write($"{matrix[i, j],2} ");
+                    matrixC[i, j] = matrixA[i, j] + matrixB[i,j];
+                    Console.Write($"{matrixC[i, j],3} ");
                 }
             }
             Console.ReadKey();
