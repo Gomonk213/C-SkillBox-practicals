44ed4325709e068b15918a6b05588392c5b9fabc
Dmitriy Smirnov <dima-smirnov-132003@mail.ru>
2023-07-31 11:43:19 +0300

    4Practical initial

---
+++ 4Practical/BothTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BothTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива (кол-во строк и столбцов)");
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            Random rand= new Random();
+            Console.WriteLine($"Матрица {rows} на {cols}:");
+            int[,] matrix = new int[rows, cols];
+            for(int i=0; i < rows; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.Next(100);
+                    Console.Write($"{matrix[i, j],2} ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
